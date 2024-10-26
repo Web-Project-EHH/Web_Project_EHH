44af3befae60b9e3cb4ecbf5c3d7d4ce4f3b5de0
Elitsa97 <elitsa.koleva.a63@learn.telerikacademy.com>
2024-10-26 16:25:23 +0300

    Refactor config.py and users_services.py

--- config.py
+++ config.py
@@ -9,3 +9,7 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 
+# JWT
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = os.getenv('ALGORITHM')
+ACCESS_TOKEN_EXPIRE_MINUTES= int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES'))
---
+++ data/models/user.py
@@ -0,0 +1,63 @@
+import datetime
+from typing import Optional
+from pydantic import BaseModel, Field, EmailStr
+
+
+class User(BaseModel):
+    id: Optional[int] = None
+    username: str = Field(..., min_length=2, max_length=50)
+    password: str
+    email: EmailStr
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    created_at: Optional[datetime.datetime] = None
+    is_admin: bool = False
+    is_deleted: bool = False
+    
+    @classmethod
+    def from_query_result(cls, id, username, password, email, first_name, last_name, created_at , is_admin, is_deleted):
+        return cls(
+            id=id,
+            username=username,
+            password=password,
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+            created_at=created_at,
+            is_admin=is_admin,
+            is_deleted=is_deleted
+        )
+    
+
+class UserLogin(BaseModel):
+    id: Optional[int] = None
+    username: str = Field(..., min_length=2, max_length=20)
+    password: str 
+    email: EmailStr
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    
+
+
+class UserResponse(BaseModel):
+    id: Optional[int] = None
+    username: str = Field(..., min_length=2, max_length=20)
+    email: EmailStr
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    is_admin: bool = False
+
+    @classmethod
+    def from_query_result(cls, query_result):
+        return cls(
+            id=query_result[0],
+            username=query_result[1],
+            email=query_result[3],
+            first_name= query_result[4],
+            last_name= query_result[5],
+            is_admin= query_result[6],
+        )
+
+class TokenResponse(BaseModel):
+    access_token: str
+    token_type: str
