175d5e8dca9b99929727f5115e088f3bba03efb1
Hristo Bonev <hristo.bonev.a63@learn.telerikacademy.com>
2024-11-10 16:04:25 +0200

    feed

--- routers/web/home.py
+++ routers/web/home.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Request
 from common.template_config import CustomJinja2Templates
+import common.auth
+from services import topics_services
 
 
 router = APIRouter(prefix='', tags=['Homepage'])
@@ -8,4 +10,6 @@ templates = CustomJinja2Templates(directory="templates")
 @router.get('/', response_model=None)
 def serve_homepage(request: Request = None):
     token = request.cookies.get('token')
-    return templates.TemplateResponse(name='index.html', request=request, context={'token': token})
+    current_user = common.auth.get_current_user(token)
+    topics = topics_services.fetch_all_topics(per_page=100)
+    return templates.TemplateResponse(name='index.html', request=request, context={'token': token, 'topics': topics})   
